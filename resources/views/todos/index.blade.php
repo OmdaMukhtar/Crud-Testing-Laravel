8529caf00efaccefad432ecf3b60b5bd7d880726
omda <abuoop123@gmail.com>
2019-09-21 01:29:37 +0200

    add style to todos list

--- resources/views/todos/index.blade.php
+++ resources/views/todos/index.blade.php
@@ -4,16 +4,23 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Todo List </div>    
+                    <div class="card-header">Todo List </div>
                     <div class="card-body">
-                        <ul>
+                        <ul class="todo-list">
                             @foreach ($todos as $todo)
-                                <li>{{ $todo->title }} {{ $todo->done }}</li>
+                                <li class="todo-list-item">
+                                    <span>{{ $loop->iteration }}</span>
+                                    <h4 class="title">
+                                        <a href="#"> {{ $todo->title }} </a>
+                                    </h4>
+                                    <span>{{ $todo->done }}</span>
+                                </li>
                             @endforeach
                         </ul>
                     </div>
-                </div>    
+                </div>
             </div>
-        </div>    
-    </div>    
+        </div>
+    </div>
 @endsection
+
