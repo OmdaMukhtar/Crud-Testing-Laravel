99dcc2b1d0cdb673644a1d8161b32dfabdba75e7
omda <abuoop123@gmail.com>
2019-09-21 16:09:10 +0200

    refactore test

--- tests/Feature/TodoListTest.php
+++ tests/Feature/TodoListTest.php
@@ -8,18 +8,21 @@ use Tests\TestCase;
 class TodoListTest extends TestCase
 {
     use DatabaseMigrations;
-    
+
     /** @test */
     public function an_authenticated_user_should_create_todo()
     {
         $user = factory('App\User')->create();
 
         $todo = factory('App\Todo')->make(['user_id' => $user->id]);
-        
+
         $this->be($user);
-        
-        $this->post('/todo', $todo->toArray())
-        ->assertSee($todo->title);
+
+        $this->post('/todo', $todo->toArray());
+
+        $this->get('/todo')
+            ->assertSee($todo->title)
+            ->assertSee($todo->done);
 
     }
 }
