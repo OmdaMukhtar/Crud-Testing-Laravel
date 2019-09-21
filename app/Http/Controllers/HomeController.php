922d53a9c326e7e57fa8cfae68293725698a6935
omda <abuoop123@gmail.com>
2019-09-21 15:59:52 +0200

    replace redirect home with todo

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -23,6 +23,8 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('home');
+        return view('todos.index')->with([
+            'todos' => \Auth::user()->todos()->get()
+        ]);
     }
 }
